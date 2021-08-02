fde3dc6ee539b36076872badb0c365d4ce8d74eb
Shon Shinkarenko <shon2789@gmail.com>
2021-08-02 20:24:09 +0300

    Added favicon modified css

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -1,4 +1,4 @@
-function openCanvas(){
+function openCanvas() {
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
-    document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');    
+    document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');
 }
