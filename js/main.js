b0c88b85ef2accd69cfee684a5aeb3cc668e510d
Shon Shinkarenko <shon2789@gmail.com>
2021-08-03 08:14:50 +0300

    Fixed project image size

--- js/main.js
+++ js/main.js
@@ -4,42 +4,42 @@ $(document).ready(onInit);
 $('.contact-btn').click(openCanvas);
 
 function onInit() {
-    $('.email-submit').click(onClickSubmit)
-    renderProjects();
-    renderModal();
+  $('.email-submit').click(onClickSubmit)
+  renderProjects();
+  renderModal();
 }
 
 function renderProjects() {
 
-    var projects = getProjectsToShow();
-    var strHTMLs = [];
-    projects.map(function (project, idx) {
-        return strHTMLs += `<div class="col-md-4 col-sm-6 portfolio-item">
+  var projects = getProjectsToShow();
+  var strHTMLs = [];
+  projects.map(function (project, idx) {
+    return strHTMLs += `<div class="col-md-4 col-sm-6 portfolio-item">
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${idx + 1}">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src="${project.image}"></ alt="">
+            <img class="img-fluid proj-image" src="${project.image}"></ alt="">
           </a>
           <div class="portfolio-caption">
             <h4>${project.name}</h4>
             <p class="text-muted">${project.title}</p>
           </div>
         </div>`
-    })
+  })
 
-    $('.portfolio').html(strHTMLs);
+  $('.portfolio').html(strHTMLs);
 }
 
 
 function renderModal() {
-    var strHTMLs = [];
-    var projects = getProjectsToShow();
+  var strHTMLs = [];
+  var projects = getProjectsToShow();
 
-    projects.map(function (proj, idx) {
-        return strHTMLs += `<div class="portfolio-modal modal fade" id="portfolioModal${idx + 1}" tabindex="-1" role="dialog" aria-hidden="true">
+  projects.map(function (proj, idx) {
+    return strHTMLs += `<div class="portfolio-modal modal fade" id="portfolioModal${idx + 1}" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="close-modal" data-dismiss="modal">
@@ -70,21 +70,21 @@ function renderModal() {
       </div>
       </div>
       </div>`
-    })
-    $('.container-modals').html(strHTMLs);
+  })
+  $('.container-modals').html(strHTMLs);
 
 }
 
 function onClickSubmit() {
-    var emailTxt = $('.email-input').val();
-    var subjectTxt = $('.subject-input').val();
-    var bodyTxt = $('.body-input').val();
-    var inputTxt = `https://mail.google.com/mail/?view=cm&fs=1&to=shon2789@gmail.com&su=${subjectTxt}&body=${bodyTxt}`
-    var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    if (!regex.test(emailTxt)) return;
+  var emailTxt = $('.email-input').val();
+  var subjectTxt = $('.subject-input').val();
+  var bodyTxt = $('.body-input').val();
+  var inputTxt = `https://mail.google.com/mail/?view=cm&fs=1&to=shon2789@gmail.com&su=${subjectTxt}&body=${bodyTxt}`
+  var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!regex.test(emailTxt)) return;
 
-    if (!emailTxt || !subjectTxt || !bodyTxt) return;
-    $('.open-new-tab').attr('href', inputTxt)
+  if (!emailTxt || !subjectTxt || !bodyTxt) return;
+  $('.open-new-tab').attr('href', inputTxt)
 
 
 
